8530da80bcdce37195a02495917ff8a7d34d4462
KimGalio <wogus5357@gmail.com>
2024-04-10 16:31:58 +0900

    seconds

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ const Home = async () => {
       <span className="text-3xl font-extrabold uppercase">To-do-app</span>
       <h1 className="text-3xl font-extrabold uppercase mb-5">
         Next.js 14
-        <span className="text-orange-700 ml-2">Server Actions</span>
+        <span className="text-orange-700 ml-2">재현 Todos</span>
       </h1>
 
       <div className="flex justify-center flex-col items-center w-[1000px]">
